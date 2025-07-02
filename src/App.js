c46fc91ab95b72474f9da9f144a8f4bbb51a002d
Ramiro Alfaro <88719821+ramijujuy@users.noreply.github.com>
2025-07-02 09:58:52 -0300

    Update App.js
    h.l

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const App = () => {
             {/* LOGO */}
             <div className="relative w-56 h-56 mx-auto">
               <img
-                src="public/logo-depiluq.jpg"
+                src={`${process.env.PUBLIC_URL}/logo-depiluq.jpg`}
                 alt="Logo"
                 className="w-full h-full rounded-full border-4 border-white shadow-2xl animate-pulse"
               />
